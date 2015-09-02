bf76099c1383cdf7f4361c0aefd1f534b9c9887a
alex285 <alexis.diavatis@hotmail.com>
2015-09-02 21:36:51 +0300

    Add a linter without rules :)

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
 
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    //.pipe(sassLint.failOnError())  //Don't fail till we test
     .pipe(gulp.dest('./dist/css'));
 });
 
